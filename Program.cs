6ef8ec425ad780de222f579f23015d489257903c
Valerian21 <72431080+Valerian21@users.noreply.github.com>
2020-10-06 17:13:02 +0900

    My First Comment

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace температура
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите свою температуру");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 37)
+            {
+                Console.WriteLine($"{name}, вам стоит сесть на больничный !!! ");
+                Console.WriteLine("Введите число от 1 до 3, что бы узнать ваш диагноз.");
+                int number = Convert.ToInt32(Console.ReadLine());
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine(" Это всего лишь ОРВИ, вам повезло!");
+                        break;
+                    case 2:
+                        Console.WriteLine("у вас ЗППП, очень жаль...");
+                        break;
+                    case 3:
+                        Console.WriteLine("Coronavirus, здоровья погибшим!!!");
+                        break;
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine($"{name}, вы можете посещать занятия! ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
